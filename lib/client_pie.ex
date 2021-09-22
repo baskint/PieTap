bbbd17a8f7d4b4765116cc31c61a4c6588e641f5
Baskin Tapkan <baskint@gmail.com>
2021-09-22 15:56:46 -0500

    disk and memory info

--- lib/client_pie.ex
+++ lib/client_pie.ex
@@ -1,3 +1,5 @@
+require Logger
+
 defmodule ClientPie do
   use GenServer
 
@@ -16,6 +18,24 @@ defmodule ClientPie do
 
   def handle_call(:get_name, _from, state) do
     {:reply, Map.get(state, :name), state}
+  end
+
+  def get_server_stats(pid, server_name) do
+    :application.start(:sasl)
+    :application.start(:os_mon)
+
+    # get disk info
+    diskInfo = :disksup.get_disk_data()
+
+    # get memory data
+    {total, allocated, {p1, p1allocated}} = :memsup.get_memory_data()
+
+    :application.stop(:os_mon)
+    :application.stop(:sasl)
+
+    IO.puts p1allocated
+    diskInfo
+
   end
 
 end
--- test/client_pie_test.exs
+++ test/client_pie_test.exs
@@ -1,10 +1,16 @@
 defmodule ClientPieTest do
   use ExUnit.Case
 
-  test "initial name is empty" do
+  test "initial name is default" do
 
     {:ok, pid} = ClientPie.start_link
 
     assert 'enver' == ClientPie.get_name(pid)
+  end
+
+  test 'status request updates the server name' do
+    {:ok, pid} = ClientPie.start_link
+
+    ClientPie.get_server_stats(pid, 'mistral')
   end
 end
